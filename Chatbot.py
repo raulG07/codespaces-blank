7bc57cefb195518e77feba221896edfd18b9c558
raulG07 <gonzalezmartinezraul295@gmail.com>
2025-02-03 18:07:14 -0600

    Added greet and goodbye

---
+++ Chatbot.py
@@ -0,0 +1,13 @@
+name = input("Hello, What is your name? ")
+orderNum = input("Whats your order number? ")
+checkOrderNum=input("Your order number is "+orderNum+" correct? ")
+if checkOrderNum == "no"
+   
+print("How may I help you "+name+"? ")
+print("1. New Order")
+print("2. Refund")
+print("3. Exit")
+option = input("")
+if option == "3":
+   print("Goodbye, have a nice day")
+   
--- ChatbotpreWork.py
+++
@@ -1,9 +0,0 @@
-name = input("What is your name? ")
-age = input("How old are you? ")
-print("Hello " + name + ", how may I help you? ")
-print("1. Schedule")
-print("2. Available products")
-print("3. Exit")
-option = input("")
-if option == "3":
-   print("Goodbye, have a nice day")
